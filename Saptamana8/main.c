349b5f89b5532015f542d262f57168ac1f3ca661
VladimirTM <90709606+VladimirTM@users.noreply.github.com>
2024-07-23 11:39:23 +0300

    Add files via upload

---
+++ Saptamana8/main.c
@@ -0,0 +1,27 @@
+#include <lpc214x.h>
+#include <stdint.h>
+#include <string.h>
+#include "serial.h"
+#include "i2c.h"
+#include "mpu6050.h"
+
+void delay(void)
+{
+	volatile unsigned int i = 0;
+	for (i = 0; i < 1200000; i++);
+}
+
+int main(void)
+{
+	initUART();
+	initI2C();
+	initMPU6050();
+	unsigned char value;
+	value = whoAmI();
+	while(1)
+	{
+		print(value);
+		delay();
+	}
+	return 0;
+}
